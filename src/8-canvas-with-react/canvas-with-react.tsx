12dc3a9e365b7938436a227e218dae234ec6003b
Sergio de la Barrera García <alu0100953275@ull.edu.es>
2026-05-01 15:17:19 +0000

    added canvas with react

---
+++ src/8-canvas-with-react/canvas-with-react.tsx
@@ -0,0 +1,31 @@
+import { useRef, useEffect } from 'react';
+import type { ReactElement } from 'react';
+import './canvas-with-react.css';
+
+export function CanvasWithReact(): ReactElement {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect((): void => {
+    const canvas = canvasRef.current;
+    if (canvas === null) return;
+    const context = canvas.getContext('2d');
+    if (context === null) return;
+
+
+    context.fillStyle = '#5C068C';
+    context.fillRect(50, 50, 150, 100);
+  }, []);
+
+  return (
+    <section className="canvas-card">
+      <h1 className="canvas-title">Canvas Example</h1>
+
+      <canvas
+        className="canvas-element"
+        ref={canvasRef}
+        width={500}
+        height={320}
+      />
+    </section>
+  );
+}
