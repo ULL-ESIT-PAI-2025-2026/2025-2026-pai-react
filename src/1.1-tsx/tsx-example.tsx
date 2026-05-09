e913c3b9703e998ce9ac12d0371d4cfb399fea0f
sarasds <alu0101484570@ull.edu.es>
2026-05-09 11:57:41 +0100

    tsx example

---
+++ src/1.1-tsx/tsx-example.tsx
@@ -0,0 +1,49 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Sara Darias Sánchez  
+ * @author Sergio de la Barrera García
+ * @author Candela García Cruz
+ * @since May 6 2026
+ * @desc 
+ */
+
+import type { JSX } from 'react';
+
+
+function BasicElement(): JSX.Element {
+	return <h1>Hello, world!</h1>;
+}
+
+function ElementWithExpression(): JSX.Element {
+  const name = 'React';
+  return <h1>Hello, {name}!</h1>;
+}
+
+function ElementWithClassName(): JSX.Element {
+  return <h1 className='greeting'>Hello, world className!</h1>;
+}
+
+function ElementWithComments(): JSX.Element {
+  return (
+    <div>
+      {/* This is a comment in JSX */}
+      <h1>Hello, world commented!</h1>
+    </div>
+  );
+}
+
+export default function App(): JSX.Element {
+	return (
+    <>
+      <BasicElement/>
+      <ElementWithExpression/>
+      <ElementWithClassName/>
+      <ElementWithComments/>
+    </>
+  );
+}
+
